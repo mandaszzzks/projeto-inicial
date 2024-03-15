7eca1f683bbf300082170ec8d41982841754ea11
3mandasouza2018@gmail.com <3mandasouza2018@gmail.com>
2024-03-15 08:24:07 -0300

    variavel nomes

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
+
+
 
 function alertar(event){
-alert("Voce clicou no botao!!!! " + event);
+alert("Voce clicou no botao" + " " + nome.value);
+
+
 
 }
-
-
+    
